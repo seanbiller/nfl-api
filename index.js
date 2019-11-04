947a4627b6726d6dfec045be68f751907ca18a19
Sean Biller <seanmbiller@gmail.com>
2019-11-03 19:40:13 -0500

    finished homework #1

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const app = express()
 app.listen(1337, () => { console.log("Listening on 1337..")})
 
 
+app.get('/teams', (request, response) => {
+    response.send(teams)
+})
 
-app.all('*', (req, res) => {
-    res.sendStatus(404)
+app.get('/teams/:filter/', (request, response) => {
+    let result = teams.filter((team) => {
+        let filter = request.params.filter
+        return team.id == filter || team.abbreviation === filter
+    })
+    response.send(result)
+})
+
+// app.get('/teams/:id/:abbreviation/', (request, response) => {
+//     let teamsByAbbreviation = teams.filter((team)=> {
+//         return team.abbreviation == request.params.abbreviation
+//     })
+//     response.send(teamsByAbbreviation)
+// })
+
+app.all('*', (request, response) => {
+    response.sendStatus(404)
 })
