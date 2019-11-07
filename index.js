c5c5a584fe84a65f498aee6a0af781cdde3a7cb1
Sean Biller <seanmbiller@gmail.com>
2019-11-07 18:40:31 -0500

    Post-team-answer

--- index.js
+++ index.js
@@ -2,27 +2,51 @@ const express = require('express')
 const teams = require('./teams.json')
 const app = express()
 
-app.listen(1337, () => { console.log("Listening on 1337..")})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on ${port}`))
 
 
 app.get('/teams', (request, response) => {
     response.send(teams)
 })
 
-app.get('/teams/:filter/', (request, response) => {
+app.get('/teams/:placeholder', (request, response) => {
     let result = teams.filter((team) => {
-        let filter = request.params.filter
-        return team.id == filter || team.abbreviation === filter
+        const filter = request.params.placeholder
+        return team.id == filter || 
+        team.location == filter ||
+        team.mascot == filter ||
+        team.abbreviation == filter ||
+        team.conference == filter ||
+        team.division == filter
     })
     response.send(result)
 })
 
-// app.get('/teams/:id/:abbreviation/', (request, response) => {
-//     let teamsByAbbreviation = teams.filter((team)=> {
-//         return team.abbreviation == request.params.abbreviation
-//     })
-//     response.send(teamsByAbbreviation)
-// })
+app.post('/teams', (request, response) => {
+    const team = {
+        id: request.body.id,
+        location: request.body.location,
+        mascot: request.body.mascot,
+        abbreviation: request.body.abbreviation,
+        conference: request.body.conference,
+        division: request.body.division
+    }
+    teams.push(team)
+    response.send(team)
+})
+// {
+//     "id": 0,
+//     "location": "0 Main St",
+//     "mascot": "Zero",
+//     "abbreviation": "zero",
+//     "conference": "AFC",
+//     "division": "East of Nowhere"
+// }
 
 app.all('*', (request, response) => {
     response.sendStatus(404)
